25eda1d8d68ceb6797f78a6cd24ebe43ebe02ca2
Edward Cacho <edward1738@gmail.com>
2025-02-20 09:34:38 -0500

    Docker config

--- ai-agent/README.md
+++ ai-agent/README.md
@@ -5,5 +5,12 @@ This is a WhatsApp AI agent to help users find the best deals on depabarato.com
 - python 3.13
 - install requirements.txt `pip install -r requirements.txt`
 
+## Environment variables
+You can get the whatsapp information from https://developers.facebook.com/apps
+- DATABASE_SQLITE_FILENAME: optional (default: "result.sqlite")
+- WHATSAPP_VERIFY_TOKEN: required
+- WHATSAPP_PHONE_NUMBER: required
+- WHATSAPP_ACCESS_TOKEN: required
+
 ## How to run
 - `python main.py`
--- ai-agent/config.py
+++ ai-agent/config.py
@@ -1,10 +1,24 @@
 import os
 import logging
+import sys
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
 
 DATABASE_SQLITE_FILENAME = os.environ.get("DB_FILENAME", "result.sqlite")
-WHATSAPP_VERIFY_TOKEN = os.environ.get("WHATSAPP_VERIFY_TOKEN", "2GASDH1341KHASDFcasdkjg324123423jkvhdf")
-WHATSAPP_PHONE_NUMBER_ID = os.environ.get("WHATSAPP_PHONE_NUMBER_ID", "1234567890")
-WHATSAPP_ACCESS_TOKEN = os.environ.get("WHATSAPP_ACCESS_TOKEN", "1234567890")
+WHATSAPP_VERIFY_TOKEN = os.environ.get("WHATSAPP_VERIFY_TOKEN")
+WHATSAPP_PHONE_NUMBER_ID = os.environ.get("WHATSAPP_PHONE_NUMBER_ID")
+WHATSAPP_ACCESS_TOKEN = os.environ.get("WHATSAPP_ACCESS_TOKEN")
+
+
+if not WHATSAPP_VERIFY_TOKEN:
+    log.error("WHATSAPP_VERIFY_TOKEN is not set")
+    sys.exit(-1)
+
+if not WHATSAPP_PHONE_NUMBER_ID:
+    log.error("WHATSAPP_PHONE_NUMBER_ID is not set")
+    sys.exit(-1)
+
+if not WHATSAPP_ACCESS_TOKEN:
+    log.error("WHATSAPP_ACCESS_TOKEN is not set")
+    sys.exit(-1)
